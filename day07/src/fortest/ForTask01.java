b8704cbac6910e1c0d044b39877219de35dd8e48
bjy5656 <qorwjddl1234@gmail.com>
2025-06-20 17:52:59 +0900

    8일차 java 커밋1

--- day07/src/fortest/ForTask01.java
+++ day07/src/fortest/ForTask01.java
@@ -46,52 +46,6 @@ public class ForTask01 {
 		}
 		
 		
-		//강사님코드
 		
-//		//2.
-//		int dan = 3;
-//		for(int i = 1; i < 10; i++) {
-//			System.out.println(dan + " x " + i + " = " + dan * i);
-//			 
-//		}
-		
-		//3.
-//		for(char i = 'A'; i <= 'F'; i++) {
-//			System.out.print(i + " ");
-//		}
-		
-		//4. 
-//		for (int i = 0; i < 6; i++) {
-//			System.out.println((char)(i + 65));
-//		}
-		
-		//5.
-		
-		for(int i = 0; i < 26; i++) {
-			if(i % 2 == 0) {
-				System.out.print((char)(i + 97));
-			} else {
-				System.out.print((char)(i + 65));
-			}
-		}
-		System.out.println();
-		for(int i = 0; i < 26; i++) {
-			System.out.print((char)(i % 2 == 0 ? i + 97 : i + 65));
-		}
-		
-		System.out.println();
-		
-		//현재 i의 값 25
-		// 	조건식 25 < 26	true, 중괄호 영역 (char)(25 % 2 == 0 false, 25 + 65) => (char)91, 증감식 i++ => 25 + 1
-		//현재 i의 값 26
-		//	조건식 26 < 26  	false, for문 탈출
-		
-		
-		//첫번재 반복문 
-		//초기식 i가 0, 조건식 0 < 26 true, 중괄호영역 (char)(97) => a, 증감식 i++ => 0 + 1
-		//현재 i의 값 1 
-		//		조건식 1 < 26 true, 중괄호영역 (char)(1 + 65) => (char)66 => 증감식 i++ => i = 1 + 1
-		//현재 i의 값 2
-		// 		조건식 2 < 26 true
 	}
 }
--- day07/src/review/Review.java
+++ day07/src/review/Review.java
@@ -110,16 +110,177 @@ public class Review {
 		// 총 부른 곡 수에 따른 보너스 곡 지급 
 		// 5곡을 부르면 1곡 추가 제공 
 		
-		// 1) 입력클래스 import 
-		Scanner sc = new Scanner(System.in);
-		// 2) 정수형 상수 2개 선언, 정수형 6개 선언(금액입력, 노랙곡수, 잔돈, 음료마실경우노래곡수, 음료마실경우잔돈, 보너스곡)
-		final int PRICE = 200, PRICE_DRINK = 100; 
-		int money = 0, song = 0, change = 0, drinkSong = 0, drinkChange = 0, bonus = 0; 
-		// 3)  입력 메시지 출력 
-		System.out.println("금액 입력 : ");
-		// 4) 금액입력 = 금액 입력 nextInt()
-		money = sc.nextInt();
-		// 5) 노래곡수 = 금액 / 상수1
+//		// 1) 입력클래스 import 
+//		Scanner sc = new Scanner(System.in);
+//		// 2) 정수형 상수 2개 선언, 정수형 6개 선언(금액입력, 노랙곡수, 잔돈, 음료마실경우노래곡수, 음료마실경우잔돈, 보너스곡)
+//		final int PRICE = 200, PRICE_DRINK = 100; 
+//		int money = 0, song = 0, change = 0, drinkSong = 0, drinkChange = 0, bonus = 0; 
+//		// 3)  입력 메시지 출력 
+//		System.out.println("금액 입력 : ");
+//		// 4) 금액입력 = 금액 입력 nextInt()
+//		money = sc.nextInt();
+//		// 5) 노래곡수 = 금액 / 상수1
+		
+		//for문만 사용 
+		//1. 100부터 1까지 출력(tab키로 띄어쓰기) 
+		//1) for문 초기식 i = 1, 조건식 i <= 100, 증감식 i++ 
+		//2) 중괄호 출력(i와 tab) 
+//		for(int i = 1; i <= 100; i++) {
+//			System.out.print(i + "\t");
+//		}
+//		
+//		System.out.println();
+//		//2. 구구단 3단만 출력 
+//		//1) for문 초기식 i = 3, 조건식 i <= 100, 증감식 i+=3 
+//		//2) 중괄호 출력(i와 tab) 
+//		int dan = 3; 
+//		for(int i = 1; i < 10; i++) {
+//			System.out.println(dan + " x " + i + " = " + dan * i);
+//		}
+//		
+//		System.out.println();
+//		//3. A ~ F 출력 (초기식 char i = 'A')
+//		//1) for문 초기식 char i = 'A', 조건식 i <= 'F'; i = i++
+//		//2) 중괄호 출력(문자형 i와 tab) 
+//		for(char i = 'A'; i <= 'F'; i = i++) {
+//			System.out.println(i + "\t");
+//		}
+		
+//		System.out.println();
+//		//5. aBcDeFgHiJkLmNoPqRsTuVwXyZ
+//		//1)  문자형 변수 소문자, 대문자 선언 
+//		//2) for문 초기식 int i = 0, 조건식 i < 26, 증감식 i+=2 
+//		//3) 중괄호 출력(문자형으로 소문자+i, 대문자+i)
+//		char lower = 'a', upper = 'B';
+//		for (int i = 0; i < 26; i += 2) {
+//			System.out.printf("%c%c", (char)(lower+i), (char)(upper+i));
+//		}
+		
+		//
+		
+		//별찍기 (전체 칸 수가 정해져있을 경우) 
+		//5번째 *출력
+		//4, 5, 6 번째 *출력
+		//3, 4, 5, 6, 7 번째 *출력
+		//...
+		
+//		for(int i = 1; i <= 5; i++) {
+//			for(int j = 1; j <= 9; j++) {
+//				int range = 9/2 +1;
+//				if (j > range - i && j < range + i ) {
+//					System.out.print("*");
+//				}else {
+//					System.out.print(" ");
+//				}
+//			}
+//			System.out.println();				
+//		}
+
+//		for (int i = 0; i < 3; i++) { // 바깥 for문 //행 
+//			System.out.print(i);
+//			for (int j = 0; j < 3; j++) { // 안쪽 for문 //열
+////				System.out.println("i : " + i + ", j : " + j);
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+//		1. 바깥 for문 시작 
+//		1-1 초기식 i를 1로 초기화
+//			조건식 i < 3 조건을 검사 => 1 < 3 true이면 바같 for문 중괄호 영역안으로 들어간다 
+//		2. 안쪽 for문 시작 
+//		2-1. 초기식 j를 1로 초기화 
+//			조건식 j < 3 조건을 검사 => 1 < 3 true이면 안쪽 for문 중괄호 영역안으로 들어간다 
+//		2-2. 출력메소드 System.out.println("i : " + i + ", j : " + j); 
+//			출력결과	=> i : 1, j : 1 
+//		3. 안쪽 for 계속 진행 
+//		3-1. 증감식으로 이동 j가 1증가(j++)	현재 j의 값 : 2 
+//		3-2. 조건식 	j < 3 조건 검사 => 2 < 3 true이면 안쪽 for문 중괄호 영역안으로 들어간다 
+//		3-3. 출력메소드 System.out.println("i : " + i + ", j : " + j);
+//			출력결과 => i  1, j : 2
+//		4. 안쪽 for문 계속 진행 
+//		4-1. 증감식으로 이동 j가 1 증가(j++) 현재 j의 값 : 3 
+//		4-2. 조건식	j < 3 조건 검사 => 3 < 3 false 이면 안쪽 for 중괄호 영역 밖으로 이동 
+//		4-3. 안쪽 for문 중롹호 밖, 바깥쪽 for문 중괄호 안의 내용이 있다면 해당 내용을 실행한다 
+//		5. 바같쪽 for문 계속 진행 
+//		5-1. 증감식이동 	i가 증가(i++)		현재 i값 : 2 
+//		5-2. 조건식	i < 3조건 검사 => 2 < 3 바깥쪽 for문 중괄호 영역안으로 들어간다 
+//		6. 안쪽 for문 시작 
+//		2~5계속 반복 
+//		+) 바같쪽 for문의 증감식으로 이동 후 조건식을 확인 시 false가 나오면 바깥 for문의 중괄호 영역 밖으로 이동한다 
+		
+//		System.out.println();
+//		
+//		for (int i = 0; i < 4; i++) { // 바깥 for문 //행 
+//			System.out.println(i);
+//			for (int j = 1; j < 5; j++) { // 안쪽 for문 //열
+//				System.out.print("i : " + i + ", j : " + j);
+//				System.out.print(i + j);
+//			}
+//			System.out.println(); // 바깥 for문 위치 
+//		}
+		
+		//2행 3열 출력하기 
+		
+//		for(int i = 1; i <= 3; i++) {
+//			for(int j = 1; j <= i; j++) {
+////				System.out.print("i = " + i + ", j = " + j + " ");
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		
+//		//사용자로부터 행과 열을 입력하여 출력하기 
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println("행과 열을 입력하세요 : ");
+//		int row = sc.nextInt();
+//		int col = sc.nextInt();
+//		for(int i = 1; i <= row; i++) {
+//			for(int j = 1; j <= col; j++) {
+////				System.out.print("i: " + i + ", j: " +_j + " ");
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 	} 
